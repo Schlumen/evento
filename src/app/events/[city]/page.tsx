b2c5b0a33cd362e6e7f30456736b3ebefeca7b4f
Schlumen <lumen.haendler@web.de>
2024-08-22 18:28:45 +0200

    Metadata

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,22 +2,31 @@ import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { capitalize } from "@/lib/utils";
+import { Metadata } from "next";
 
-type EventPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function Events({ params }: EventPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
+  const city = params.city;
+
+  return {
+    title: city === "all" ? "All Events" : `Events in ${capitalize(city)}`,
+  };
+}
+
+export default async function Events({ params }: Props) {
   const city = params.city;
 
   return (
     <main className="flex min-h-[110vh] flex-col items-center px-[20px] py-24">
       <H1 className="mb-28">
         {city === "all" && "All Events"}
-        {city !== "all" &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
       <Suspense fallback={<Loading />}>
         <EventsList city={city} />
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,18 +1,11 @@
 import { EventoEvent } from "@/lib/types";
 import EventCard from "./event-card";
+import { getEvents } from "@/lib/utils";
 
 type EventsListProps = { city: string };
 
 export default async function EventsList({ city }: EventsListProps) {
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
-    {
-      next: {
-        revalidate: 300,
-      },
-    },
-  );
-  const events: EventoEvent[] = await response.json();
+  const events = await getEvents(city);
 
   return (
     <section className="flex max-w-[1100px] flex-wrap justify-center gap-10 px-[20px]">
